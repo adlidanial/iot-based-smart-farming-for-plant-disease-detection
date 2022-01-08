b18653f8b8101491902a0cc75d5937b50a2ae0e3
adlidanial <56016225+adlidanial@users.noreply.github.com>
2022-01-08 17:32:07 +0800

    update

--- routes/connect.py
+++ routes/connect.py
@@ -1,13 +1,13 @@
+"""""
 # Development
 HOST = "localhost"
 USER = "root"
 PASSWORD = ""
 DB = "plant_disease_detection"
+"""
 
-"""""
 # Production
 HOST = "us-cdbr-east-05.cleardb.net"
 USER = "b1646d0d089bab"
 PASSWORD = "94ed5161"
 DB = "heroku_bd37171be9fe3d6"
-"""
